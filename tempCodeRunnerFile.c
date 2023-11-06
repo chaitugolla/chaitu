f563fd5ddd06168c5af683ebae7c7f546e8b47a9
chaitugolla <chaitanyagolla4934@gmail.com>
2023-11-06 17:15:59 +0530

    datatype.c

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 int main()
 {
-    int a,b;
-    a=24;
-    b=11;
-    printf("a+b:%d\n",a+b);
-    printf("a-b:%d\n",a-b);
-    printf("a*b:%d\n",a*b);
-    printf("a/b:%d\n",a/b);
-    return 0;
+	int a = 9;
+	int b = -9;
+    float d=34.99;
+    char f='chaitu';
+	int c = 89U;
+	long int d = 99998L;
+    printf("Integer value with positive data: %d\n", a);
+	printf("Integer value with negative data: %d\n", b);
+    printf("Integer value with an float:%f\n",d);
+    printf("Integer value with an char data %c\n",f);
+	printf("Integer value with an unsigned int data: %u\n",
+		c);
+	printf("Integer value with an long int data: %ld", d);
 
+	return 0;
 }

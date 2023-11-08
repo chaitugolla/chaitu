ef95a56e78bbcf5cc28f1bd0a4c59772ff46f374
chaitugolla <chaitanyagolla4934@gmail.com>
2023-11-08 11:30:57 +0530

    relatoin2.c

---
+++ relation2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter a value:");
+    scanf("%d",&a);
+    printf("enter b value:");
+    scanf("%d",&b);
+    if(a==b)
+    {
+        printf("both are same values");
+    }
+    else{
+        printf("both are not same");
+    }
+    return 0;
+
+}
